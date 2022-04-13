3e1f20be059ba6bddb88cfb24ef20154e1b58c7a
Vishal Wagh <vishal.wagh@usbank.com>
2022-04-13 18:56:55 +0530

    create essential feed framework.

---
+++ EssentailFeed/EssentailFeed/EssentailFeed.h
@@ -0,0 +1,18 @@
+//
+//  EssentailFeed.h
+//  EssentailFeed
+//
+//  Created by Wagh, Vishal on 13/04/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EssentailFeed.
+FOUNDATION_EXPORT double EssentailFeedVersionNumber;
+
+//! Project version string for EssentailFeed.
+FOUNDATION_EXPORT const unsigned char EssentailFeedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EssentailFeed/PublicHeader.h>
+
+
