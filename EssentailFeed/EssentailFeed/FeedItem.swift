9cbc695bce4741e9cd8f2b1306331496c697d1a5
Vishal Wagh <vishal.wagh@usbank.com>
2022-04-13 18:58:20 +0530

    create the FeedItem model

---
+++ EssentailFeed/EssentailFeed/FeedItem.swift
@@ -0,0 +1,16 @@
+//
+//  FeedItem.swift
+//  EssentailFeed
+//
+//  Created by Wagh, Vishal on 13/04/22.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let descriptiom: String?
+    let location: String?
+    let imageURL: URL
+}
+
