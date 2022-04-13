267a6ad020f36e0c21a329885f9031c88c674881
Vishal Wagh <vishal.wagh@usbank.com>
2022-04-14 00:53:17 +0530

    Request data from URL on load

--- EssentailFeed/EssentailFeedTests/RemoteFeedLoaderTests.swift
+++ EssentailFeed/EssentailFeedTests/RemoteFeedLoaderTests.swift
@@ -8,10 +8,15 @@
 import XCTest
 
 class RemoteFeedLoader {
-    
+    func load() {
+        HTTPClient.shared.requestURL = URL(string: "https://a-url.com")
+    }
 }
 
 class HTTPClient {
+    static let shared = HTTPClient()
+    
+    private init() {}
     var requestURL: URL?
 }
 
@@ -19,10 +24,19 @@ class RemoteFeedLoaderTests: XCTestCase {
     
    
     func test_init_doesNotRequestDataFromURL(){
-        let client = HTTPClient()
+        let client = HTTPClient.shared
         _ = RemoteFeedLoader()
         
         XCTAssertNil(client.requestURL)
     }
     
+    func test_load_requestDataFromURL() {
+        
+        let client = HTTPClient.shared
+        let sut = RemoteFeedLoader()
+        
+        sut.load()
+        
+        XCTAssertNotNil(client.requestURL)
+    }
 }
