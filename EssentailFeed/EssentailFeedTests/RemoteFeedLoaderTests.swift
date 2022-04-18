07d044913ef80043849039981f6c448deb7010bb
Vishal Wagh <vishal.wagh@usbank.com>
2022-04-18 10:36:31 +0530

    Add moe task assertions for different HTTP status code

--- EssentailFeed/EssentailFeedTests/RemoteFeedLoaderTests.swift
+++ EssentailFeed/EssentailFeedTests/RemoteFeedLoaderTests.swift
@@ -49,12 +49,17 @@ class RemoteFeedLoaderTests: XCTestCase {
     func test_load_deliversErrorOnNon200HttpResponse(){
         let (sut, client) = makeSut()
         
-        var capturedErrors = [RemoteFeedLoader.Error]()
-        sut.load { capturedErrors.append($0) }
+        let sample = [199, 201, 300, 400, 500]
         
-        client.complete(withStatusCode: 400)
-        
-        XCTAssertEqual(capturedErrors, [.invalidData])
+        sample.enumerated().forEach { index, code in
+            
+            var capturedErrors = [RemoteFeedLoader.Error]()
+            sut.load { capturedErrors.append($0) }
+            
+            client.complete(withStatusCode: code, at: index)
+            
+            XCTAssertEqual(capturedErrors, [.invalidData])
+        }
     }
     
     // MARK: - Helpers
