31b4028e16bc35779b60b58ff70d6795a28e6907
Vishal Wagh <vishal.wagh@usbank.com>
2022-04-18 10:30:18 +0530

    Delivers invalidData error on non 200 HTTP response

--- EssentailFeed/EssentailFeedTests/RemoteFeedLoaderTests.swift
+++ EssentailFeed/EssentailFeedTests/RemoteFeedLoaderTests.swift
@@ -46,6 +46,17 @@ class RemoteFeedLoaderTests: XCTestCase {
         XCTAssertEqual(capturedErrors, [.conectivity])
     }
     
+    func test_load_deliversErrorOnNon200HttpResponse(){
+        let (sut, client) = makeSut()
+        
+        var capturedErrors = [RemoteFeedLoader.Error]()
+        sut.load { capturedErrors.append($0) }
+        
+        client.complete(withStatusCode: 400)
+        
+        XCTAssertEqual(capturedErrors, [.invalidData])
+    }
+    
     // MARK: - Helpers
     
     private func makeSut(url: URL = URL(string: "https://a-url.com")!) -> (sut: RemoteFeedLoader,client: HTTPClientSpy) {
@@ -55,18 +66,29 @@ class RemoteFeedLoaderTests: XCTestCase {
     }
     
     private class HTTPClientSpy: HTTPClient {
-        var messages = [(url: URL, completion: (Error) -> Void)]()
+        var messages = [(url: URL, completion: (Error?, HTTPURLResponse?) -> Void)]()
         
         var requestURLs : [URL] {
             return messages.map { $0.url }
         }
 
-        func get(from url: URL, completion: @escaping (Error) -> Void) {
+        func get(from url: URL, completion: @escaping (Error?, HTTPURLResponse?) -> Void) {
             messages.append((url,completion))
         }
         
         func complete(with error: Error, at index: Int = 0){
-            messages[index].completion(error)
+            messages[index].completion(error, nil)
+        }
+        
+        func complete(withStatusCode code: Int, at index: Int = 0){
+            let response = HTTPURLResponse(
+                url: requestURLs[index],
+                statusCode: code,
+                httpVersion: nil,
+                headerFields: nil
+            )
+            
+            messages[index].completion(nil, response)
         }
     }
 
