0fa01e159f656e3f62cc78a0119a3badd2ea1df0
Vishal Wagh <vishal.wagh@usbank.com>
2022-04-20 10:25:30 +0530

    simplify type declaration with helper function

--- EssentailFeed/EssentailFeedTests/FeedAPI/RemoteFeedLoaderTests.swift
+++ EssentailFeed/EssentailFeedTests/FeedAPI/RemoteFeedLoaderTests.swift
@@ -37,7 +37,7 @@ class RemoteFeedLoaderTests: XCTestCase {
     func test_load_deliversErrorOnClientError(){
         let (sut, client) = makeSut()
         
-        expect(sut, toCompleteWith: .failure(RemoteFeedLoader.Error.conectivity)) {
+        expect(sut, toCompleteWith: failure(.conectivity)) {
             let clientError = NSError(domain: "Test", code: 0)
             client.complete(with: clientError)
         }
@@ -49,7 +49,7 @@ class RemoteFeedLoaderTests: XCTestCase {
         let sample = [199, 201, 300, 400, 500]
         
         sample.enumerated().forEach { index, code in
-            expect(sut, toCompleteWith: .failure(RemoteFeedLoader.Error.invalidData)) {
+            expect(sut, toCompleteWith: failure(.invalidData)) {
                 
                 let json = makeItemsJSON([])
                 client.complete(withStatusCode: code, data: json, at: index)
@@ -60,7 +60,7 @@ class RemoteFeedLoaderTests: XCTestCase {
     func test_load_deliversErrorOn200HTTPResposeWithIvalidJSON(){
         let (sut, client) = makeSut()
         
-        expect(sut, toCompleteWith: .failure(RemoteFeedLoader.Error.invalidData)) {
+        expect(sut, toCompleteWith: failure(.invalidData)) {
             let invalidJSON = Data("invalid JSON".utf8)
                 client.complete(withStatusCode: 200, data: invalidJSON)
         }
@@ -119,6 +119,10 @@ class RemoteFeedLoaderTests: XCTestCase {
         TrackMemoryLeaks(client, file: file, line: line)
         
         return (sut, client)
+    }
+    
+    private func failure(_ error: RemoteFeedLoader.Error) -> RemoteFeedLoader.Result {
+        return .failure(error)
     }
     
     private func TrackMemoryLeaks(_ instance: AnyObject, file: StaticString = #filePath, line: UInt = #line) {
