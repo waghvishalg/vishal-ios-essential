10fc921ad21150fd3c4c81541e4c617928631ce7
Vishal Wagh <vishal.wagh@usbank.com>
2022-04-21 09:17:39 +0530

    Remove redundant test

--- EssentailFeed/EssentailFeedTests/FeedAPI/URLSessionHTTPClientTests.swift
+++ EssentailFeed/EssentailFeedTests/FeedAPI/URLSessionHTTPClientTests.swift
@@ -20,16 +20,6 @@ class URLSessionHTTPClient {
 }
 
 class URLSessionHTTPClientTests: XCTestCase {
-
-    func test_getFromURL_createsDataTaskWithURL() {
-        let url = URL(string: "http://any-url.com")!
-        let session = URLSessionSpy()
-        let sut = URLSessionHTTPClient(session: session)
-        
-        sut.get(from: url)
-        
-        XCTAssertEqual(session.receivedURL, [url])
-    }
     
     func test_getFromURL_resumeDataTaskWithURL() {
         let url = URL(string: "http://any-url.com")!
@@ -48,7 +38,6 @@ class URLSessionHTTPClientTests: XCTestCase {
     // MARK: - Helpers
     
     private class URLSessionSpy: URLSession {
-        var receivedURL = [URL]()
         private var stubs = [URL: URLSessionDataTask]()
         
         func stub(url: URL, task: URLSessionDataTask){
@@ -56,7 +45,6 @@ class URLSessionHTTPClientTests: XCTestCase {
         }
         
         override func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
-            receivedURL.append(url)
             return stubs[url] ?? FakeURLSessionDataTask()
         }
     }
