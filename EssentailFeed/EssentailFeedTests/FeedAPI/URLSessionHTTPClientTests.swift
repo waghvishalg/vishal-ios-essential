b5aaeb3b9e902f25315a2f4d039a8907d493f1d5
Vishal Wagh <vishal.wagh@usbank.com>
2022-04-23 18:07:51 +0530

    Move URLSessionHTTPClient to its own file in production

--- EssentailFeed/EssentailFeedTests/FeedAPI/URLSessionHTTPClientTests.swift
+++ EssentailFeed/EssentailFeedTests/FeedAPI/URLSessionHTTPClientTests.swift
@@ -8,29 +8,6 @@
 import XCTest
 import EssentailFeed
 
-class URLSessionHTTPClient: HTTPClient {
-    private let session: URLSession
-    
-    init(session: URLSession = .shared){
-        self.session = session
-    }
-    
-    
-    struct UnexpectedValuesRepresention: Error {}
-    
-    func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
-        session.dataTask(with: url) { data, response, error in
-            if let error = error {
-                completion(.failure(error))
-            }else if let data = data, let response = response as? HTTPURLResponse {
-                completion(.success(data, response))
-            }else {
-                completion(.failure(UnexpectedValuesRepresention()))
-            }
-        }.resume()
-    }
-}
-
 class URLSessionHTTPClientTests: XCTestCase {
     
     
