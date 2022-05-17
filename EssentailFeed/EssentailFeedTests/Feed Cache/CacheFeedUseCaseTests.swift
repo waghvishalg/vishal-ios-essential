c2b5645ff7860fcfe48ac073f1738032321f286c
Vishal Wagh <waghvishalg@gmail.com>
2022-05-17 09:12:42 +0530

    Save command Cache request deletions

--- EssentailFeed/EssentailFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ EssentailFeed/EssentailFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -8,15 +8,20 @@
 import XCTest
 import EssentailFeed
 
-
 class LocalFeedLoader {
+    
+    private let store: FeedStore
     init(store: FeedStore) {
-        
+        self.store = store
+    }
+    
+    func save(_ items: [FeedItem]){
+        store.deleteCacheFeedCount += 1
     }
 }
 
 class FeedStore {
-    var deleteCacheCount = 0
+    var deleteCacheFeedCount = 0
 }
 
 class CacheFeedUseCaseTests: XCTestCase {
@@ -25,6 +30,25 @@ class CacheFeedUseCaseTests: XCTestCase {
         let store = FeedStore()
         _ = LocalFeedLoader(store: store)
         
-        XCTAssertEqual(store.deleteCacheCount, 0)
+        XCTAssertEqual(store.deleteCacheFeedCount, 0)
+    }
+    
+    func test_save_requestCacheDeletion(){
+        let store = FeedStore()
+        let sut = LocalFeedLoader(store: store)
+        let items = [uniqueItem(),uniqueItem()]
+        sut.save(items)
+        
+        XCTAssertEqual(store.deleteCacheFeedCount, 1)
+    }
+    
+    //MARK: - Helper
+    
+    private func uniqueItem() -> FeedItem {
+        return FeedItem(id: UUID(), description: "any desc", location: "any loc", imageURL: anyURL())
+    }
+    
+    private func anyURL() -> URL{
+       return URL(string: "http://any-url.com")!
     }
 }
