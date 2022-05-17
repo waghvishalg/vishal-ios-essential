de93b6c997f6d29e68c84c118ed820108ffb36b2
Vishal Wagh <waghvishalg@gmail.com>
2022-05-17 09:48:10 +0530

    extract system under test creation to a factory method to protect tests against breaking changes

--- EssentailFeed/EssentailFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ EssentailFeed/EssentailFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -27,15 +27,12 @@ class FeedStore {
 class CacheFeedUseCaseTests: XCTestCase {
     
     func test_init_doesNotDeleteCacheUponCreation(){
-        let store = FeedStore()
-        _ = LocalFeedLoader(store: store)
-        
+        let (_, store) = makeSUT()
         XCTAssertEqual(store.deleteCacheFeedCount, 0)
     }
     
     func test_save_requestCacheDeletion(){
-        let store = FeedStore()
-        let sut = LocalFeedLoader(store: store)
+        let (sut, store) = makeSUT()
         let items = [uniqueItem(),uniqueItem()]
         sut.save(items)
         
@@ -44,6 +41,11 @@ class CacheFeedUseCaseTests: XCTestCase {
     
     //MARK: - Helper
     
+    private func makeSUT() -> (sut: LocalFeedLoader,store: FeedStore){
+        let store = FeedStore()
+        let sut = LocalFeedLoader(store: store)
+        return (sut, store)
+    }
     private func uniqueItem() -> FeedItem {
         return FeedItem(id: UUID(), description: "any desc", location: "any loc", imageURL: anyURL())
     }
