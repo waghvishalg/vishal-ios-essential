99fe25280148b90f3f7bd2a0570d08eb3538e806
Vishal Wagh <waghvishalg@gmail.com>
2022-05-18 08:42:40 +0530

    Guarantee  'LocalFeedLoader' does not deliver insertion after instance has been deallocated.

--- EssentailFeed/EssentailFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ EssentailFeed/EssentailFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -22,7 +22,10 @@ class LocalFeedLoader {
             guard let self = self else { return }
             
             if error == nil {
-                self.store.insert(items, timestamp: self.currentDate(), completion: completion)
+                self.store.insert(items, timestamp: self.currentDate()) { [weak self] error in
+                    guard self != nil else { return }
+                    completion(error)
+                }
             }else {
                 completion(error)
             }
@@ -115,6 +118,20 @@ class CacheFeedUseCaseTests: XCTestCase {
         sut = nil
         store.completeDeletion(with: anyNSError())
         
+        XCTAssertTrue(receivedResult.isEmpty)
+    }
+    
+    func test_save_doesNotDeliverInsetionErrorAfterSUTInstanceHasBeenDeallocated() {
+        let store = FeedStoreSpy()
+        var sut: LocalFeedLoader? = LocalFeedLoader(store: store, currentDate: Date.init)
+        
+        var receivedResult = [Error?]()
+        sut?.save([uniqueItem()]) { receivedResult.append($0) }
+        
+        store.completeDeletionSuccessfully()
+        store.completeInsetion(with: anyNSError())
+        sut = nil
+
         XCTAssertTrue(receivedResult.isEmpty)
     }
     
