418b4c06a65a79cf21d7f1483b2b3faa87d90000
Vishal Wagh <waghvishalg@gmail.com>
2022-06-01 22:33:54 +0530

    Move FeedStore to its own file.

---
+++ EssentailFeed/EssentailFeed/Feed Cache/FeedStore.swift
@@ -0,0 +1,16 @@
+//
+//  FeedStore.swift
+//  EssentailFeed
+//
+//  Created by Vishal Wagh on 01/06/22.
+//
+
+import Foundation
+
+public protocol FeedStore {
+    typealias DeletionCompletion = (Error?) -> Void
+    typealias InsertionCompletion = (Error?) -> Void
+
+    func deletionCacheFeed(completion: @escaping DeletionCompletion)
+    func insert(_ items: [FeedItem],timestamp: Date, completion: @escaping InsertionCompletion)
+}
