f582f7d251de93697c8bf61d8124ad67ad6dff29
Vishal Wagh <waghvishalg@gmail.com>
2022-06-01 22:17:10 +0530

    invert the if login to make code paths easier to follow.

--- EssentailFeed/EssentailFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ EssentailFeed/EssentailFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -21,13 +21,13 @@ class LocalFeedLoader {
         store.deletionCacheFeed { [weak self] error in
             guard let self = self else { return }
             
-            if error == nil {
+            if let error = error {
+                completion(error)
+            }else {
                 self.store.insert(items, timestamp: self.currentDate()) { [weak self] error in
                     guard self != nil else { return }
                     completion(error)
                 }
-            }else {
-                completion(error)
             }
         }
     }
@@ -129,8 +129,8 @@ class CacheFeedUseCaseTests: XCTestCase {
         sut?.save([uniqueItem()]) { receivedResult.append($0) }
         
         store.completeDeletionSuccessfully()
-        store.completeInsetion(with: anyNSError())
         sut = nil
+        store.completeInsetion(with: anyNSError())
 
         XCTAssertTrue(receivedResult.isEmpty)
     }
