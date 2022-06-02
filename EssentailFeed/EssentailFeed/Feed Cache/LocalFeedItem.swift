67d866e70d0ef3b6e174b7394eb380613496c355
Vishal Wagh <waghvishalg@gmail.com>
2022-06-02 08:17:26 +0530

    Move 'LocalFeedtem' to its own file

---
+++ EssentailFeed/EssentailFeed/Feed Cache/LocalFeedItem.swift
@@ -0,0 +1,22 @@
+//
+//  LocalFeedItem.swift
+//  EssentailFeed
+//
+//  Created by Vishal Wagh on 02/06/22.
+//
+
+import Foundation
+
+public struct LocalFeedItem: Equatable{
+    public let id: UUID
+    public let description: String?
+    public let location: String?
+    public let imageURL: URL
+    
+    public init(id: UUID, description: String?, location: String?, imageURL: URL) {
+        self.id = id
+        self.description = description
+        self.location = location
+        self.imageURL = imageURL
+    }
+}
