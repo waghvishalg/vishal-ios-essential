8a3c01446bdaf7af2d4b9e15b41c483ea5b9c554
Vishal Wagh <waghvishalg@gmail.com>
2022-06-05 11:44:33 +0530

    make 'FeedCachePolicy' static since it doesn't keep any state.

--- EssentailFeed/EssentailFeed/Feed Cache/LocalFeedLoader.swift
+++ EssentailFeed/EssentailFeed/Feed Cache/LocalFeedLoader.swift
@@ -8,13 +8,13 @@
 import Foundation
 
 private final class FeedCachePolicy {
-    private let calendar = Calendar(identifier: .gregorian)
+    private static let calendar = Calendar(identifier: .gregorian)
     
-    private var maxCacheAgeInDays: Int {
+    private static var maxCacheAgeInDays: Int {
         return 7
     }
     
-    func validate(_ timeStamp: Date, against date: Date) -> Bool {
+    static func validate(_ timeStamp: Date, against date: Date) -> Bool {
         guard let maxCacheAge = calendar.date(byAdding: .day, value: maxCacheAgeInDays, to: timeStamp) else {
             return false
         }
@@ -26,7 +26,6 @@ public final class LocalFeedLoader {
     
     private let store: FeedStore
     private let currentDate: () -> Date
-    private let cachePolicy = FeedCachePolicy()
 
     public init(store: FeedStore, currentDate: @escaping () -> Date) {
         self.store = store
@@ -68,7 +67,7 @@ extension LocalFeedLoader: FeedLoader {
             case let .failure(error):
                 completion(.failure(error))
                 
-            case let .found(feed, timeStamp) where self.cachePolicy.validate(timeStamp, against: self.currentDate()):
+            case let .found(feed, timeStamp) where FeedCachePolicy.validate(timeStamp, against: self.currentDate()):
                 completion(.success(feed.toModels()))
                 
             case .found, .empty:
@@ -88,7 +87,7 @@ extension LocalFeedLoader {
             case .failure:
                 self.store.deletionCacheFeed{ _ in }
             
-            case let .found(_ , timestamp) where !self.cachePolicy.validate(timestamp, against: self.currentDate()):
+            case let .found(_ , timestamp) where !FeedCachePolicy.validate(timestamp, against: self.currentDate()):
                 self.store.deletionCacheFeed{ _ in }
             
             case .empty, .found: break
