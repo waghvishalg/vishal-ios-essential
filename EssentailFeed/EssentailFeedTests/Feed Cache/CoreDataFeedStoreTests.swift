97dbef4ca1bd8b955ed4de2adfeac461c9bd950e
Vishal Wagh <waghvishalg@gmail.com>
2022-07-05 17:37:28 +0530

    Add empty CoreData feed store specs

---
+++ EssentailFeed/EssentailFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
@@ -0,0 +1,92 @@
+//
+//  CoreDataFeedStoreTests.swift
+//  EssentailFeedTests
+//
+//  Created by Vishal Wagh on 05/07/22.
+//
+
+import XCTest
+import EssentailFeed
+
+class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
+    
+    func test_retrieve_deliversEmptyOnEmptyCache() {
+
+    }
+    
+    func test_retrieve_hasNoSideEffectOnEmptyCache() {
+
+    }
+    
+    func test_retrieve_deliversFoundValueOnNonEmptyCache() {
+        
+    }
+    
+    func test_retrieve_hasNoSideEffectsOnNonEmptyCache() {
+        
+    }
+    
+    func test_retrieve_deliversFailureOnRetirevalError() {
+        
+    }
+    
+    func test_retrieve_hasNoSideEffectsOnFailure() {
+        
+    }
+    
+    func test_insert_deliversNoErrorOnEmptyCache() {
+        
+    }
+    
+    func test_insert_deliversNoErrorOnNonEmptyCache() {
+        
+    }
+    
+    func test_insert_overridesPreviouslyInsertedCacheValues() {
+        
+    }
+    
+    func test_insert_deliversErrorOnInsertionError() {
+        
+    }
+    
+    func test_insert_hasNoSideEffectsOnInsertonError() {
+        
+    }
+    
+    func test_delete_deliversNoErrorOnEmptyCache() {
+        
+    }
+    
+    func test_delete_hasNoSideEffectOnEmptyCache(){
+        
+    }
+    
+    func test_delete_deliversNoErrorOnNonEmptyCache(){
+        
+    }
+    
+    func test_delete_emptiesPreviouslyInsertedCache() {
+        
+    }
+    
+    func test_delete_deliversErrorOnDeletionError() {
+        
+    }
+    
+    func test_delete_hasNoSideEffectOnDeletionError() {
+        
+    }
+    
+    func test_storeSideEffects_runSerially(){
+        
+    }
+    
+    // - MARK: Helpers
+    
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> FeedStore {
+        let sut = CoreDataFeedStore()
+        trackMemoryLeaks(sut, file: file, line: line)
+        return sut
+    }
+}
