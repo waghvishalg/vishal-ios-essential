4c4580efe81e797316e246b6f17f9a61ef834ff4
Vishal Wagh <waghvishalg@gmail.com>
2022-07-08 10:59:07 +0530

    CoreDataFeedStore.deleteCachedFeed() has no side-effects on empty cache

--- EssentailFeed/EssentailFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
+++ EssentailFeed/EssentailFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
@@ -67,7 +67,8 @@ class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
     }
     
     func test_delete_hasNoSideEffectOnEmptyCache(){
-        
+        let sut = makeSUT()
+        assertThatDeleteHasNoSideEffectsOnEmptyCache(on: sut)
     }
     
     func test_delete_deliversNoErrorOnNonEmptyCache(){
