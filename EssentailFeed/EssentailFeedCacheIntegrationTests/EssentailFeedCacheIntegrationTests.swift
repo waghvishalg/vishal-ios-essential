980bc5ab23d7c918c747a1b3bd619f27941e7bc3
Vishal Wagh <waghvishalg@gmail.com>
2022-07-13 09:56:37 +0530

    Add 'EssentailFeedCacheIntegrationTests' target to separate the potentially-slower cache integration test from the fast unit/isolated tests.

---
+++ EssentailFeed/EssentailFeedCacheIntegrationTests/EssentailFeedCacheIntegrationTests.swift
@@ -0,0 +1,12 @@
+//
+//  EssentailFeedCacheIntegrationTests.swift
+//  EssentailFeedCacheIntegrationTests
+//
+//  Created by Vishal Wagh on 13/07/22.
+//
+
+import XCTest
+
+class EssentailFeedCacheIntegrationTests: XCTestCase {
+
+}
