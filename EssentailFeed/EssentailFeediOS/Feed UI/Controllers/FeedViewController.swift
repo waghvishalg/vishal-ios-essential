f86c490d6e94cc9c077fe1aee61745b981b971dc
Vishal Wagh <waghvishalg@gmail.com>
2023-03-31 14:43:20 +0530

    move main thread dispatch queue to presenter.

--- EssentailFeed/EssentailFeediOS/Feed UI/Controllers/FeedViewController.swift
+++ EssentailFeed/EssentailFeediOS/Feed UI/Controllers/FeedViewController.swift
@@ -26,15 +26,7 @@ final public class FeedViewController: UITableViewController, UITableViewDataSou
     var delegate : FeedViewControllerDelegate?
     
     var tableModel = [FeedImageCellController]() {
-        didSet {
-            if Thread.isMainThread {
-                tableView.reloadData()
-            } else {
-                DispatchQueue.main.async { [weak self] in
-                    self?.tableView.reloadData() 
-                }
-            }
-        }
+        didSet { tableView.reloadData() }
     }
     
     public override func viewDidLoad() {
@@ -47,10 +39,6 @@ final public class FeedViewController: UITableViewController, UITableViewDataSou
         delegate?.didRequestFeedRefresh()
     }
     func display(_ viewModel: FeedLoadingViewModel) {
-        
-        guard Thread.isMainThread else {
-            return DispatchQueue.main.async { [weak self] in self?.display(viewModel) }
-        }
         
         if viewModel.isLoading {
             refreshControl?.beginRefreshing()
